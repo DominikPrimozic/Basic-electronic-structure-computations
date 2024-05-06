c26622bdc6da4f299305b52d44109422cd5fb8eb
DominikPrimozic <168119284+DominikPrimozic@users.noreply.github.com>
2024-05-06 08:55:39 +0200

    Update make_csv_grid.py

--- C++/make_csv_grid.py
+++ C++/make_csv_grid.py
@@ -7,11 +7,24 @@ class molecule():
     def __init__(self,charge,position):
         self.charges=np.array(charge)
         self.position=position
-        
-charge=[1,1,8]
-centers=[ np.array([0, 1.4305227, 1.1092692]), 
-          np.array([0, -1.4305227, 1.1092692]),
-          np.array([0, 0, 0])] 
+
+choice=input("He,H2 or H2O :")
+if choice=="He":
+    #He
+    charge=[2]
+    centers=[ np.array([0, 0,0])]
+elif choice=="H2":
+    #H2
+    charge=[1,1]
+    centers=[ np.array([0, 0,0]), 
+              np.array([0,0,1.4])] 
+elif choice=="H2O":
+    #H2O
+    charge=[1,1,8]
+    centers=[ np.array([0, 1.4305227, 1.1092692]), 
+              np.array([0, -1.4305227, 1.1092692]),
+              np.array([0, 0, 0])] 
+else: raise ValueError
 
 mole=molecule(charge,centers)
 
